a6dcb95a0054922c03c45b42bc2b00ce942dd5b6
msisaif <msi132043@gmail.com>
2024-04-14 20:50:25 +0600

    Icon & Logo Update option : Build

---
+++ public/assets/ExamQuestions-b86cbbf1.js
@@ -0,0 +1,1 @@
+import{_ as e,o as n,b as o}from"./index-cf53effb.js";const s={};function t(c,r){return n(),o("div",null,"Exam Questions")}const _=e(s,[["render",t]]);export{_ as default};
---
+++ public/assets/ExamRoutine-2b7c848d.js
@@ -0,0 +1,1 @@
+import{_ as e,o as n,b as o}from"./index-ed18a14c.js";const t={};function c(r,a){return n(),o("div",null,"Exam Routine")}const _=e(t,[["render",c]]);export{_ as default};
