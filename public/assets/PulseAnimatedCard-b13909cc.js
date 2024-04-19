a617460dc109fa8eb6db45ed58d3fddd2b49c58b
msisaif <msi132043@gmail.com>
2024-04-20 00:37:18 +0600

    build: academic subject and academic class

---
+++ public/assets/PulseAnimatedCard-b13909cc.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./PulseAnimatedBox-68fc1868.js";import{o as a,b as t,e as c,f as s}from"./index-3ce42378.js";const r={class:"w-full flex flex-wrap justify-center items-center gap-4"},i={__name:"PulseAnimatedCard",setup(l){return(o,_)=>(a(),t("div",null,[c("div",r,[s(e,{class:"w-full h-6"}),s(e,{class:"w-20 h-8"})])]))}};export{i as _};
